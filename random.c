98d9c49e4b40f024c6bcdfdaaf215c6a1e3ceda9
robnava <robnava@gmail.com>
2023-05-04 00:46:46 -0500

    First Commit

---
+++ random.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <sys/random.h>
+
+int
+randnum (int maxval)
+{
+	int randval;
+
+	getrandom(&randval, sizeof(int), GRND_NONBLOCK);
+
+	if (randval < 0) {
+		return (-1 * randval % maxval + 1);
+	}
+	else {
+		return (randval % maxval + 1);
+	}
+}
+
+int
+main(void)
+{
+	int number;
+	int guess;
+
+	number = randnum(100);
+
+	puts("Guess a number between 1 and 100");
+
+	do {
+		scanf("%d", &guess);
+
+		if (guess < number ) {
+			puts("Too low.");
+		}
+		else if (guess > number) {
+			puts("Too high");
+		}
+	} while (guess != number);
+
+	puts("That's right!");
+
+	return 0;
+}
